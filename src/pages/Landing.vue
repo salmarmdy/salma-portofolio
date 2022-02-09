44484d6746a38c03f39df60fd0355366497c0466
Salma Five-Code <salma@five-code.com>
2022-02-09 15:47:12 +0700

    code

---
+++ src/pages/Landing.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <div class="page-header page-header-small">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('img/bg6.jpg')"
+      >
+      </parallax>
+      <div class="content-center">
+        <div class="container">
+          <h1 class="title">Knowledge Management WIKA.</h1>
+        </div>
+      </div>
+    </div>
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="separator separator-primary"></div>
+        <div class="section-story-overview">
+          <div class="row">
+            <div class="col-md-6">
+              <div
+                class="image-container image-left"
+                style="background-image: url('img/km.png')"
+              >
+                <!-- First image on the left side -->
+                <p class="blockquote blockquote-primary">
+                  "Over the span of the satellite record, Arctic sea ice has
+                  been declining significantly, while sea ice in the
+                  Antarctichas increased very slightly"
+                  <br />
+                  <br />
+                  <small>-NOAA</small>
+                </p>
+              </div>
+            </div>
+            <div class="col-md-5">
+              <!-- First image on the right side, above the article -->
+              <div
+                class="image-container image-right"
+                style="background-image: url('img/km.png')"
+              ></div>
+              <h3>
+                So what does the new record for the lowest level of winter ice
+                actually mean
+              </h3>
+              <p>
+                The Arctic Ocean freezes every winter and much of the sea-ice
+                then thaws every summer, and that process will continue whatever
+                happens with climate change. Even if the Arctic continues to be
+                one of the fastest-warming regions of the world, it will always
+                be plunged into bitterly cold polar dark every winter. And
+                year-by-year, for all kinds of natural reasons, there’s huge
+                variety of the state of the ice.
+              </p>
+              <p>
+                For a start, it does not automatically follow that a record
+                amount of ice will melt this summer. More important for
+                determining the size of the annual thaw is the state of the
+                weather as the midnight sun approaches and temperatures rise.
+                But over the more than 30 years of satellite records, scientists
+                have observed a clear pattern of decline, decade-by-decade.
+              </p>
+              <p>
+                The Arctic Ocean freezes every winter and much of the sea-ice
+                then thaws every summer, and that process will continue whatever
+                happens with climate change. Even if the Arctic continues to be
+                one of the fastest-warming regions of the world, it will always
+                be plunged into bitterly cold polar dark every winter. And
+                year-by-year, for all kinds of natural reasons, there’s huge
+                variety of the state of the ice.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--<div class="section section-contact-us text-center">
+      <div class="container">
+        <h2 class="title">Want to work with us?</h2>
+        <p class="description">Your project is very important to us.</p>
+        <div class="row">
+          <div class="col-lg-6 text-center ml-auto mr-auto col-md-8">
+            <fg-input
+              class="input-lg"
+              placeholder="First Name..."
+              v-model="form.firstName"
+              addon-left-icon="now-ui-icons users_circle-08"
+            >
+            </fg-input>
+            <fg-input
+              class="input-lg"
+              placeholder="Email Here..."
+              v-model="form.email"
+              addon-left-icon="now-ui-icons ui-1_email-85"
+            >
+            </fg-input>
+            <div class="textarea-container">
+              <textarea
+                class="form-control"
+                name="name"
+                rows="4"
+                cols="80"
+                v-model="form.message"
+                placeholder="Type a message..."
+              ></textarea>
+            </div>
+            <div class="send-button">
+              <n-button type="primary" round block size="lg"
+                >Send Message</n-button
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>-->
+  </div>
+</template>
+<script>
+import { Button, FormGroupInput } from "@/components";
+export default {
+  name: "landing",
+  bodyClass: "landing-page",
+  components: {
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput,
+  },
+  data() {
+    return {
+      form: {
+        firstName: "",
+        email: "",
+        message: "",
+      },
+    };
+  },
+};
+</script>
+<style></style>
---
+++ src/pages/Profile.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <div class="page-header clear-filter" filter-color="orange">
+      <parallax
+        class="page-header-image"
+        style="background-image: url('img/s1.jpg')"
+      >
+      </parallax>
+      <div class="container">
+        <div class="photo-container">
+          <img src="img/salma4.png" alt="" />
+        </div>
+        <h3 class="title">Salma Ramadhiany</h3>
+        <p class="category">Programmer</p>
+        <div class="content">
+          <div class="social-description">
+            <h2>6</h2>
+            <p>Project</p>
+          </div>
+          <div class="social-description">
+            <h2>3 year</h2>
+            <p>Experience</p>
+          </div>
+          <div class="social-description">
+            <h2>1</h2>
+            <p>Company</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <h3 class="title">About me</h3>
+        <h5 class="description">
+          IT professional with over 3 year of experience web developer for
+          startup. Develop project concepts and maintain optimal workflow
+        </h5>
+      </div>
+    </div>
+    <div class="container">
+      <h3 class="title">EXPERTISE</h3>
+      <div class="col-md-12">
+        <n-progress type="info" :value="90" label="PHP" show-value>
+        </n-progress>
+        <n-progress type="success" :value="95" label="Laravel" show-value>
+        </n-progress>
+        <n-progress type="primary" :value="75" label="Node JS" show-value>
+        </n-progress>
+        <n-progress type="warning" :value="80" label="Vue JS" show-value>
+        </n-progress>
+        <n-progress type="primary" :value="80" label="Strapi" show-value>
+        </n-progress>
+        <n-progress type="warning" :value="80" label="HTML" show-value>
+        </n-progress>
+        <n-progress type="info" :value="75" label="CSS" show-value>
+        </n-progress>
+        <n-progress type="success" :value="80" label="JS" show-value>
+        </n-progress>
+        <n-progress type="primary" :value="80" label="Postgre SQL" show-value>
+        </n-progress>
+        <n-progress type="warning" :value="80" label="Mysql" show-value>
+        </n-progress>
+      </div>
+    </div>
+
+    <div class="section section-about-us">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">Work Experience</h2>
+          </div>
+        </div>
+        <div class="separator separator-primary"></div>
+        <div class="section-story-overview">
+          <div class="row">
+            <div class="col-md-6">
+              <div
+                class="image-container image-left"
+                style="background-image: url('img/fivecode3.jpeg')"
+              >
+                <!-- First image on the left side -->
+                <!--<p class="blockquote blockquote-primary">
+                  "Over the span of the satellite record, Arctic sea ice has
+                  been declining significantly, while sea ice in the
+                  Antarctichas increased very slightly"
+                  <br />
+                  <br />
+                  <small>-NOAA</small>
+                </p>-->
+              </div>
+            </div>
+            <div class="col-md-5">
+              <!-- First image on the right side, above the article -->
+              <div
+                class="image-container image-right"
+                style="background-image: url('img/fivecode1.jpeg')"
+              ></div>
+              <h3>PT Lima Kode Teknologi</h3>
+              <p>
+                working as a web programmer, from 2019 until now. Using php,
+                laravel, jquery, CSS, lumen, JS, mysql, POSTGRE SQL, vue JS &
+                node JS
+              </p>
+              <p>
+                Work with teams to build projects. Develop project concepts and
+                maintain an optimal workflow. Develop frontend and backend
+                systems. Maintain database and collaborate with team using
+                gitlab
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container text-center">
+      <h3 class="title my-5">Porfolio</h3>
+      <div class="row">
+        <div class="col-md-6">
+          <router-link to="/landing" target="_blank">
+            <img src="img/km.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/landing"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page
+          </router-link>
+        </div>
+        <div class="col-md-6">
+          <router-link to="/profile" target="_blank">
+            <img src="img/eg.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page</router-link
+          >
+        </div>
+        <div class="col-md-6">
+          <router-link to="/profile" target="_blank">
+            <img src="img/m1.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page</router-link
+          >
+        </div>
+        <div class="col-md-6">
+          <router-link to="/profile" target="_blank">
+            <img src="img/edu2.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page</router-link
+          >
+        </div>
+        <div class="col-md-6">
+          <router-link to="/profile" target="_blank">
+            <img src="img/dress.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page</router-link
+          >
+        </div>
+        <div class="col-md-6">
+          <router-link to="/profile" target="_blank">
+            <img src="img/shamo3.png" alt="Image" class="img-raised" />
+          </router-link>
+          <router-link
+            to="/profile"
+            class="btn btn-simple btn-primary btn-round"
+            >View Page</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { Tabs, TabPane, Progress } from "@/components";
+
+export default {
+  components: {
+    [Progress.name]: Progress,
+  },
+  name: "profile",
+  bodyClass: "profile-page",
+  data() {
+    return {
+      pagination: {
+        simple: 1,
+        default: 2,
+        full: 3,
+      },
+    };
+  },
+};
+</script>
+<style></style>
